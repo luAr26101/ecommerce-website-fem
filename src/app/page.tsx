b9735f50ca14ee68db529cfe9d5397d50e7d57c4
luAr26101 <raul.cosmin.26@icloud.com>
2026-02-17 07:12:50 +0200

    Add prettier plugin for tailwind and and main colors for the project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,25 +2,25 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
-      <main className='flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start'>
+    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between bg-white px-16 py-32 sm:items-start dark:bg-black">
         <Image
-          className='dark:invert'
-          src='/next.svg'
-          alt='Next.js logo'
+          className="dark:invert"
+          src="/next.svg"
+          alt="Next.js logo"
           width={100}
           height={20}
           priority
         />
         <Image
-          className='dark:invert'
-          src='/assets/home/desktop/image-hero.jpg'
-          alt='Pattern circles'
+          className="dark:invert"
+          src="/assets/home/desktop/image-hero.jpg"
+          alt="Pattern circles"
           width={1440}
           height={729}
         />
         <h1>Hello, Next.js</h1>
-        <p>
+        <p className="random-class text-primary text-2xl">
           Lorem ipsum dolor, sit amet consectetur adipisicing elit. Consectetur
           eveniet iure a sapiente dolor labore dignissimos quis, earum autem
           atque odio voluptatum debitis, numquam quasi ducimus cumque omnis
